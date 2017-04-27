003fce1f9a22d9ef0ef99ccfccf56dd8d7e18769
Tobias Knopp <tobias@knoppweb.de>
2017-04-27 10:42:55 +0200

    minor change

--- src/Custom.jl
+++ src/Custom.jl
@@ -1,9 +1,9 @@
 export apply1DAcquisition
 
-function apply1DAcquisition(ip)
+function apply1DAcquisition(ip, numSamplesPerPeriod=100, numPeriods=100)
   socket = connect(ip,7777)
-  write(socket,UInt32(100))
-  write(socket,UInt32(100))
+  write(socket,UInt32(numSamplesPerPeriod))
+  write(socket,UInt32(numPeriods))
 
   u=read(socket,Int16, 100*100*2)
   u = reshape(u,100*100,2)
