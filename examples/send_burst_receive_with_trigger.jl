e8b498a772fbab74659ea88806af4d3ebada9908
Matthias Gräser <ma.graeser@uke.de>
2020-02-23 13:25:13 +0100

    added burst mode

--- examples/send_burst_receive_with_trigger.jl
+++ examples/send_burst_receive_with_trigger.jl
@@ -2,12 +2,12 @@ using Redpitaya
 using PyPlot
 using FFTW
 
-rp = RedPitaya("10.167.6.139")
+rp = RedPitaya("192.168.178.66")
 
 freq = 25000.0
 dec = optimalDecimation(rp,freq)
 #freq=25000
-numPeriods = 30
+numPeriods = 5
 freqR = roundFreq(rp,dec,freq)
 numSampPerPeriod = numSamplesPerPeriod(rp,dec,freqR)
 numSamp = numSampPerPeriod*numPeriods
@@ -22,10 +22,10 @@ println("Number Sampling Points per Period: $numSampPerPeriod")
 send(rp,"GEN:RST")
 #send(rp,"SOUR1:BURS:STAT ON")
 #change this to SQUARE to get a SQUARE excitation
-sendAnalogSignal(rp,1,"SINE",freqR,0.4,offset=0.2,ncycBurst=60)
+sendAnalogBurstSignal(rp,1,"SINE",freqR,0.4,offset=0.2,ncycBurst=numPeriods+1,repetitions=2,repetitionCycleTime=1/freq*10*1e6)
 #send(rp,"SOUR1:VOLT:OFFS 0")
 # receive data
-trigger = "AWG_NE" # "CH1_PE"
+trigger = "AWG_PE" # "CH1_PE"
 u1 = receiveAnalogSignalWithTrigger(rp, 1, 0, numSamp, dec=dec, delay=0.2, typ="OLD",
                       trigger=trigger, triggerLevel=-0.0, binary=true, raw=false,
                       triggerDelay=numSampPerPeriod)
@@ -42,3 +42,17 @@ semilogy(abs.(rfft(u1)),"o-b",lw=2)
 figure(4)
 plot(1:length(u1),u1,"x-r",lw=2)
 plot(length(u1)+1:2*length(u1),u1,"x-b",lw=2)
+send(rp,"GEN:RST")
+
+trapez=trapeziod(0.9)
+sendAnalogBurstSignal(rp,1,"ARBITRARY",freqR,0.4,offset=0.2,ncycBurst=5+1,repetitions=2,repetitionCycleTime=1/freq*10*1e6,AWG_data=trapez)
+#send(rp,"SOUR1:VOLT:OFFS 0")
+# receive data
+trigger = "AWG_PE" # "CH1_PE"
+u1 = receiveAnalogSignalWithTrigger(rp, 1, 0, numSamp*4, dec=dec, delay=0.0, typ="OLD",
+                      trigger=trigger, triggerLevel=-0.1, binary=true, raw=false,
+                      triggerDelay=0*numSampPerPeriod)
+
+figure(5)
+subplot(2,1,1)
+plot(u1)
--- src/Generator.jl
+++ src/Generator.jl
@@ -1,26 +1,82 @@
-export sendAnalogSignal, disableAnalogOutput
+export sendAnalogSignal, disableAnalogOutput, sendAnalogBurstSignal, trapeziod
 
 ### Analog Output Interface ###
 
 function sendAnalogSignal(rp::RedPitaya, chan::Integer, func::String,
-                          freq::Number, amplitude::Number, ncycBurst::Integer=0)
-  if !in(func,["SINE", "SQUARE", "TRIANGLE", "SAWU", "SAWD", "PWD"])
+                          freq::Number, amplitude::Number; offset::Number=0,AWG_data::Array=[])
+  if !in(func,["SINE", "SQUARE", "TRIANGLE", "SAWU", "SAWD", "PWD", "AWG"])
     error("waveform $func not supported")
   end
   source = "SOUR$(chan)"
-  #send(rp,"GEN:RST")
+  send(rp,"GEN:RST")
   send(rp,"$(source):FUNC $(func)") # Set function of output signal
   send(rp,"$(source):FREQ:FIX $(freq)") # Set frequency of output signal
   send(rp,"$(source):VOLT $(amplitude)") # Set amplitude of output signal
-
-  if ncycBurst > 0
-    #send(rp,"$(source):BURS:STAT ON") #Set burst mode to ON
-    #bufferSize(rp)
-    send(rp,"$(source):BURS:NCYC $ncycBurst") #Set 1 pulses of sine wave
+  send(rp,"$(source):VOLT:OFFS $(offset)") # Set offset of output signal
+  if func=="AWG"
+    send(rp, "$(source):TRAC:DATA:DATA $(AWG_data)")
   end
   send(rp,"OUTPUT$(chan):STATE ON") # Set output to ON
 end
 
+function sendAnalogBurstSignal(rp::RedPitaya, chan::Integer, func::String,
+                          freq::Number, amplitude::Number; offset::Number=0,
+                          ncycBurst::Integer=0, repetitions::Integer=0,
+                          repetitionCycleTime::Float64=0 , AWG_data::Array=[])
+  if !in(func,["SINE", "SQUARE", "TRIANGLE", "SAWU", "SAWD", "PWD", "ARBITRARY"])
+    error("waveform $func not supported")
+  end
+  source = "SOUR$(chan)"
+  send(rp,"GEN:RST")
+  send(rp,"$(source):FUNC $(func)") # Set function of output signal
+  send(rp,"$(source):FREQ:FIX $(freq)") # Set frequency of output signal
+  send(rp,"$(source):VOLT $(amplitude)") # Set amplitude of output signal
+  send(rp,"$(source):VOLT:OFFS $(offset)") # Set offset of output signal
+  if ncycBurst > 0
+   println("BurstMode On. Sending $(ncycBurst) Bursts")
+   println("$(source):BURS:NCYC $ncycBurst") 
+
+   send(rp,"$(source):BURS:STAT ON") #Set burst mode to ON
+  #bufferSize(rp)
+   send(rp,"$(source):BURS:NCYC $ncycBurst") #Set 1 pulses of sine wave
+   if repetitions!=0
+     send(rp, "$(source):BURS:NOR $repetitions")
+     println("$(repetitionCycleTime)")
+     send(rp, "$(source):BURS:INT:PER $(repetitionCycleTime)")
+   end
+ end
+   if func=="ARBITRARY"
+    AWG_data=join(AWG_data, ",")
+    send(rp, "$(source):TRAC:DATA:DATA $(AWG_data)")
+  end
+  send(rp,"OUTPUT$(chan):STATE ON") # Set output to ON
+end
+
+
 function disableAnalogOutput(rp::RedPitaya, chan::Integer)
   send(rp,"OUTPUT$(chan):STATE OFF") # Set output to ON
 end
+function trapeziod(flankratio; len=2^14, offset=0)
+    x = range(0, stop=2*pi, length=len)     #generates clean periodic signal
+
+    rise = x * 2/(1-flankratio)      #calc width of rising edge and mirror it ...
+    fall = - rise
+    edge_point = trunc(Int, (1-flankratio)/2 * len)
+    top = trunc(Int, (2^14 - 4*edge_point ) / 2 )
+
+    p1 = collect( rise[1 : edge_point] / (2*pi))
+    p2 = collect( ones(top))
+    p3 = collect( (fall[1 : 2*edge_point] / (2*pi) .+ 1))
+    p4 = collect( -ones(top))
+    p5 = collect( rise[1 : edge_point] / (2*pi) .- 1)
+
+    trapez = vcat(p1,p2,p3,p4,p5).-offset
+
+     if length(trapez) != len   #test
+        error("ERROR: failed to concatenate array parts correctly, $(len) $(length(trapez))")
+    end
+return trapez
+end
+
+
+
